f6c72ab757c8cb46fa388a9efee1d171c5d8c980
CreatorSiSo <creatorsisohd@gmail.com>
2022-09-30 06:35:56 +0000

    Beans task finished.

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,44 @@
-let beans = 0
-let water = 0
+let beans = 0;
+let water = 0;
 
 const getCoffee = () => {
-  beans = Math.max(beans - 2, 0)
-  water = Math.max(water - 5., 0)
-}
+	const [beansFuture, waterFuture] = [beans - 2, water - 5];
+	if (!(beansFuture < 0 || waterFuture < 0)) {
+		beans = beansFuture;
+		water = waterFuture;
+	}
+};
 
 const refill = () => {
-  beans += 20
-  water += 45
-}
+	beans += 20;
+	water += 45;
+};
 
-const coffeeButton = document.querySelector("#coffee_button") as HTMLButtonElement
-const refillButton = document.querySelector("#refill_button") as HTMLButtonElement
-const beansStatus = document.querySelector("#beans_status") as HTMLElement
-const waterStatus = document.querySelector("#water_status") as HTMLElement
+const coffeeButton = document.querySelector(
+	"#coffee_button"
+) as HTMLButtonElement;
+const refillButton = document.querySelector(
+	"#refill_button"
+) as HTMLButtonElement;
+const beansStatus = document.querySelector("#beans_status") as HTMLElement;
+const waterStatus = document.querySelector("#water_status") as HTMLElement;
 
 const updateText = () => {
-  beansStatus.textContent = beans.toString()
-  waterStatus.textContent = water.toString()
-}
+	beansStatus.textContent = beans.toString();
+	waterStatus.textContent = water.toString();
+};
 
 coffeeButton.addEventListener("click", (e) => {
-  e.preventDefault
-  getCoffee()
-  updateText()
-})
+	e.preventDefault;
+	getCoffee();
+	updateText();
+});
 refillButton.addEventListener("click", (e) => {
-  e.preventDefault
-  refill()
-  updateText()
-})
+	e.preventDefault;
+	refill();
+	updateText();
+});
 
-updateText()
+updateText();
 
-export { }
+export {};
