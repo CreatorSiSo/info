3b83668ac06fa1707b1c53607f3d2aec233f84fa
CreatorSiSo <creatorsisohd@gmail.com>
2022-10-07 06:34:31 +0000

    Lk html/js.

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,25 @@
-// Ein zylinderförmiger Behälter vom Durchmesser d1(m) und der Höhe h(m) wird durch einen Schlauch mit dem Durchmesser d2(cm) mit Wein gefüllt.
-// Die Durchflussgeschwindigkeit ist v (m/s).
-// Durch ein Programm soll die Zeit ermittelt werden, die zum vollständigen Füllen des Behälters benötigt wird.
-
-// VZ = PI * r * r * h
-
-// VS = PI * r * r * l          | l = vS * t
-// VS = PI * r * r * vS * t
-
-// VZ = VS
-// PI * rZ * rZ * h = PI * rS * rS * vS * t  | /PI
-// rZ * rZ * h = rS * rS * vS * t            | /(rS * rS * vS)
-// t = (rZ * rZ * h) / (rS * rS * vS)
-
-const calcTime = (h: number, rZ: number, rS: number, vS: number) =>
-	(rZ * rZ * h) / (rS * rS * vS);
-
-const heightInput = document.querySelector("#height") as HTMLInputElement;
-const dCylindInput = document.querySelector("#d_cylinder") as HTMLInputElement;
-const dTubeInput = document.querySelector("#d_tube") as HTMLInputElement;
-const speedInput = document.querySelector("#speed") as HTMLInputElement;
+const bruttoInput = document.querySelector("#brutto") as HTMLInputElement;
+const rentInput = document.querySelector("#rent") as HTMLInputElement;
 
 const calcButton = document.querySelector("#calc_button") as HTMLButtonElement;
-const timeStatus = document.querySelector("#time_status") as HTMLElement;
+
+const netto1Status = document.querySelector("#netto_1_status") as HTMLElement;
+const netto2Status = document.querySelector("#netto_2_status") as HTMLElement;
+const nettoStatus = document.querySelector("#netto_status") as HTMLElement;
+const restStatus = document.querySelector("#rest_status") as HTMLElement;
 
 calcButton.addEventListener("click", (e) => {
 	e.preventDefault;
-	const h = parseFloat(heightInput.value);
-	const rZ = parseFloat(dCylindInput.value) / 2;
-	const rS = parseFloat(dTubeInput.value) / 200;
-	const vS = parseFloat(speedInput.value);
-	timeStatus.textContent = calcTime(h, rZ, rS, vS).toString() + "s";
+	const brutto = parseFloat(bruttoInput.value);
+	const netto1 = brutto - brutto * 0.19;
+	const netto2 = brutto - brutto * 0.05;
+	const netto = netto1 - netto1 * 0.05;
+	const rest = netto - parseFloat(rentInput.value);
+
+	netto1Status.textContent = netto1.toString() + "€";
+	netto2Status.textContent = netto2.toString() + "€";
+	nettoStatus.textContent = netto.toString() + "€";
+	restStatus.textContent = rest.toString() + "€";
 });
 
 export {};
