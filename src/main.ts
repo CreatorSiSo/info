d7ebd4abff78aa22b1100b057c3d0a3629f53c0d
CreatorSiSo <creatorsisohd@gmail.com>
2022-10-14 06:39:08 +0000

    Lisas Eis.

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,22 @@
-const bruttoInput = document.querySelector("#brutto") as HTMLInputElement;
-const rentInput = document.querySelector("#rent") as HTMLInputElement;
+const numIceInput = document.querySelector("#num_ice") as HTMLInputElement;
+const ateWhiteIceInput = document.querySelector(
+	"#ate_white_ice"
+) as HTMLInputElement;
 
 const calcButton = document.querySelector("#calc_button") as HTMLButtonElement;
 
-const netto1Status = document.querySelector("#netto_1_status") as HTMLElement;
-const netto2Status = document.querySelector("#netto_2_status") as HTMLElement;
-const nettoStatus = document.querySelector("#netto_status") as HTMLElement;
-const restStatus = document.querySelector("#rest_status") as HTMLElement;
+const whiteIceValue = document.querySelector("#white_ice_value") as HTMLElement;
+const darkIceValue = document.querySelector("#dark_ice_value") as HTMLElement;
 
 calcButton.addEventListener("click", (e) => {
 	e.preventDefault;
-	const brutto = parseFloat(bruttoInput.value);
-	const netto1 = brutto - brutto * 0.19;
-	const netto2 = brutto - brutto * 0.05;
-	const netto = netto1 - netto1 * 0.05;
-	const rest = netto - parseFloat(rentInput.value);
+	const numIce = parseFloat(numIceInput.value);
+	const ateWhiteIce = parseFloat(ateWhiteIceInput.value);
+	const numWhiteIce = Math.floor(Math.max(numIce * 0.45 - ateWhiteIce, 0));
+	const numDarkIce = Math.floor(Math.max(numIce * 0.55 - 2 * ateWhiteIce, 0));
 
-	netto1Status.textContent = netto1.toString() + "€";
-	netto2Status.textContent = netto2.toString() + "€";
-	nettoStatus.textContent = netto.toString() + "€";
-	restStatus.textContent = rest.toString() + "€";
+	whiteIceValue.textContent = (numWhiteIce * 1.2).toString() + "€";
+	darkIceValue.textContent = (numDarkIce * 0.9).toString() + "€";
 });
 
 export {};
