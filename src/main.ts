ba964b0bd49b311d517de8cef88cbb5aaf571603
CreatorSiSo <creatorsisohd@gmail.com>
2023-01-06 08:05:27 +0000

    06.01.2023

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,77 @@
 const form = document.querySelector("form") as HTMLFormElement;
-const amountInput = document.querySelector(
-	"#amount_bottles_input"
+const number1Input = document.querySelector(
+	"#number_1_input"
 ) as HTMLInputElement;
+const number2Input = document.querySelector(
+	"#number_2_input"
+) as HTMLInputElement;
+const operatorSelect = document.querySelector(
+	"#op_select"
+) as HTMLSelectElement;
+
 const resultElement = document.querySelector("#result") as HTMLElement;
 
 form.addEventListener("submit", (e) => {
 	e.preventDefault();
-	let amount = parseFloat(amountInput.value);
+	resultElement.textContent = "Result: ";
 
-	resultElement.textContent = "";
+	const number1 = parseFloat(number1Input.value);
+	const number2 = parseFloat(number2Input.value);
 
-	for (; amount > 0; amount -= 1) {
-		resultElement.textContent +=
-			amount === 1
-				? "Nur noch eine Flasche Cola im Kühlschrank\n"
-				: `${amount} Flaschen im Kühlschrank!\n`;
+	const operation = operatorSelect.value;
+
+	switch (operation) {
+		case "diff":
+			resultElement.textContent += (
+				number1 > number2 ? number1 - number2 : number2 - number1
+			).toString();
+			break;
+		case "mul":
+			resultElement.textContent += (number1 * number2).toString();
+			break;
+		case "div":
+			if (number2 === 0) {
+				resultElement.textContent = "Error: Cannot divide by 0";
+				return;
+			}
+			resultElement.textContent += (number1 / number2).toString();
+			break;
+		case "sms":
+			const discriminant = (number1 + number2) / (number1 * number2);
+			const sqrt = Math.sqrt(discriminant);
+			if (isNaN(sqrt) || !isFinite(sqrt)) {
+				resultElement.textContent =
+					"Error: Inputs cannot be 0 or both negative";
+			} else {
+				resultElement.textContent += sqrt.toString();
+			}
+			break;
+		case "rem":
+			resultElement.textContent +=
+				number1 % number2 === 0
+					? `${number1} is divisible by ${number2}`
+					: `${number1} is not divisible by ${number2}`;
+			break;
+		case "sum":
+			const [numbers, numbers_sum] = sum(number1, number2);
+			resultElement.textContent += `Sum of [${numbers}] is ${numbers_sum}`;
+			break;
+		default:
+			resultElement.textContent = "Error: Invalid Operation";
+			return;
 	}
-
-	resultElement.textContent += "Alle Flaschen sind ausgetrunken!\n";
 });
+
+function sum(number1: number, number2: number): [number[], number] {
+	const start = (number1 < number2 ? number1 : number2) + 1;
+	let numbers = [];
+	for (let i = start; i < (number1 + number2) / 2; i += 1) {
+		numbers.push(i);
+	}
+	if (numbers.length === 0) {
+		numbers.push(0);
+	}
+	return [numbers, numbers.reduce((accum, num) => accum + num)];
+}
 
 export {};
