427e4417540aaa2f8627e99f1d39a2ab3e4dd0d6
Simon Sommer <64036709+CreatorSiSo@users.noreply.github.com>
2023-01-20 08:55:13 +0100

    LK 3 Kalendar

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,25 @@
 const form = document.querySelector("form") as HTMLFormElement;
+const year_input = document.querySelector("#year_input") as HTMLInputElement;
 const month_input = document.querySelector("#month_input") as HTMLSelectElement;
 const result = document.querySelector("#result") as HTMLPreElement;
 
 form.addEventListener("submit", (e) => e.preventDefault());
 
+year_input.value = new Date(Date.now()).getFullYear().toString();
 printCalendar();
+year_input.addEventListener("input", printCalendar);
 month_input.addEventListener("change", printCalendar);
 
 function printCalendar() {
-	result.textContent = calendar_string(2022, parseInt(month_input.value));
+	result.textContent = calendar_string(
+		parseInt(year_input.value),
+		parseInt(month_input.value)
+	);
 }
 
 function calendar_string(year: number, month: number): string {
 	const month_length = days_in_month(year, month);
-	const first_day = new Date(year, month - 1, 1).getDay();
+	const first_day = new Date(year, month, 1).getUTCDay();
 	const data = [
 		"Montag",
 		"Dienstag",
@@ -38,9 +44,9 @@ function range_inclusive(start: number, end: number): number[] {
 	return Array.from({ length: end - start + 1 }, (_, i) => i + start);
 }
 
-function* chunks<T>(arr: T[], n: number): Generator<T[], void> {
-	for (let i = 0; i < arr.length; i += n) {
-		yield arr.slice(i, i + n);
+function* chunks<T>(array: T[], chunk_size: number): Generator<T[], void> {
+	for (let i = 0; i < array.length; i += chunk_size) {
+		yield array.slice(i, i + chunk_size);
 	}
 }
 
